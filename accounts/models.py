9f902008951ccb899769192e72f71c304bf3c9d9
narae18 <starestarstar@naver.com>
2023-08-14 13:04:06 +0900

    feat: 회원가입보완

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,7 @@
 from django.db import models
-
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-
 
 
 class Profile(models.Model):
